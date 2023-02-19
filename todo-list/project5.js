d071160d10931209cb7e8a5db4982f4977a5eca0
Pranav V <pranav9708@gmail.com>
2023-02-19 15:40:53 +0530

    todolist setup complete

---
+++ todo-list/project5.js
@@ -0,0 +1,13 @@
+//ToDoList/ Master
+
+// Key Concepts for Project
+
+// document.getElementById()
+// addEventListener()
+// e.target
+// appendChild();
+// createElement();
+// parentElement();
+// remove()
+// classList.add();
+
