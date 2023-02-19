dc81df6aa94a9188fc40eb90722653c525c228cf
Pranav V <pranav9708@gmail.com>
2023-02-19 10:17:29 +0530

    event propagation and event delegation

--- Basics/index.js
+++ Basics/index.js
@@ -1,16 +1,18 @@
-// DOM Manipulation
+//DOM Manipulation 
 
-// Event Listeners
+//Event Probagation
+// Three phases
+// 1.Event Capturing- Event trickles down from document to html to body to .. target
+// 2.Target- triggering of event takes place
+// 3.Event Bubbling- travels back up the Dom from target to ..to body to html to document
 
-const revealBtn= document.querySelector('.reveal-btn');
-const hiddenContent= document.querySelector('.hidden-content');
+//adddeventlistener("click",function(),true);  true means we need event capturing and false means event bubbling
+//e.stopPropagation(); - useful function to stop propagation 
+//{once:true}; -fire off only once
 
-function revealContent(){
-    if(hiddenContent.classList.contains('reveal-btn')){
-        hiddenContent.classList.remove('reveal-btn');
-    }else{
-        hiddenContent.classList.add('reveal-btn');
-    }
-}
 
-revealBtn.addEventListener('click',revealContent);
+//Event Delegation
+//it allows users to append a single event listener to a parent that
+//adds it to all of it s present and future descendants that match a selector. 
+
+//adding event listener to parent and then using event.target to apply the neccessary changes to child elements
