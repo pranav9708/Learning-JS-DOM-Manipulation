0a50f253ec020b7b51ccb8eb399ab282fabd50ee
Pranav V <pranav9708@gmail.com>
2023-02-19 17:22:30 +0530

    todolist functionalities completed

--- todo-list/project5.js
+++ todo-list/project5.js
@@ -11,3 +11,54 @@
 // remove()
 // classList.add();
 
+//variables
+
+const addTask=document.getElementById('add-task');
+const taskContainer=document.getElementById('task-container');
+const inputTask=document.getElementById('input-task');
+
+//eventListener for add button
+
+addTask.addEventListener('click',function(){
+    let task=document.createElement('div');
+    task.classList.add('task');
+
+    let li=document.createElement('li');
+    li.innerText=`${inputTask.value}`;
+    task.appendChild(li);
+
+    let checkButton=document.createElement('button');
+    checkButton.innerHTML=`<i class="fa-solid fa-check"></li>`
+    checkButton.classList.add('checkTask');
+    task.appendChild(checkButton);
+
+    let deleteButton=document.createElement('button');
+    deleteButton.innerHTML=`<i class="fa-solid fa-trash"></li>`
+    deleteButton.classList.add('deleteTask');
+    task.appendChild(deleteButton);
+
+    if(inputTask.value===""){
+        alert("please Enter a task");
+    }else{
+        taskContainer.appendChild(task);
+    }
+    inputTask.value = "";
+
+    let isChecked = false;
+    checkButton.addEventListener("click", function(){
+        if(!isChecked){
+            checkButton.parentElement.style.textDecoration="line-through";
+            isChecked=true;
+        }else{
+            checkButton.parentElement.style.textDecoration="none";
+            isChecked=false;
+        }
+    })
+
+    deleteButton.addEventListener("click", function(e){
+        let target=e.target;
+        //parent->li, parent of li->task
+        target.parentElement.parentElement.remove();
+    })
+
+})
